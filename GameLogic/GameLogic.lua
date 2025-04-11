853c075f22f92b81678b5f88b9a917b5368a7f32
Ivan <ivan0.111@mail.ru>
2025-04-11 22:12:56 +0700

    add data file

--- GameLogic/GameLogic.lua
+++ GameLogic/GameLogic.lua
@@ -1,16 +1,26 @@
 function onLoad()
-    addHotkey("Take Damage", function(playerColor) if(playerColor == "Black") then takeDamage(self.getDescription()) end end)
+    colorPlayer = "White"
+    addHotkey("Take Damage", function(playerColor) if(playerColor == "Black") then takeDamage() end end)
 end
 
-function takeDamage(colorPlayer)
+function takeDamage()
     local player = {} player = Global.getVar("saveInfoPlayer")[colorPlayer]
     player.Health.current = player.Health.current - 1
     Global.call("setUI", colorPlayer) Global.call("updateSave")
 end
 
 function changeRace(player, race)
-    local colorPlayer = self.getDescription()
     local player = {} player = Global.getVar("saveInfoPlayer")[colorPlayer]
     player.Race = race
     Global.call("changeRaceBonus", colorPlayer) Global.call("setUI", colorPlayer)
+end
+
+function changeClass(player, class)
+    local player = {} player = Global.getVar("saveInfoPlayer")[colorPlayer]
+    player.Class = class
+    Global.call("setUI", colorPlayer)
+end
+
+function changeColor(player, color)
+    colorPlayer = color
 end
