9f63364bf9d3e012657690b5b681b5e6ce1bd769
Ivan <ivan0.111@mail.ru>
2025-04-15 11:59:09 +0700

    add new base game logic function

--- GameLogic/GameLogic.lua
+++ GameLogic/GameLogic.lua
@@ -1,55 +1,95 @@
 function onLoad()
-    colorPlayer = "White"
+    bodyHitTable = {
+        chest = 30, shoulder = 20, legs = 15, arm = 20, feets = 10, head = 5
+    }
+    selectedColorPlayer = "White"
     addHotkey("Take Damage", function(playerColor) if(playerColor == "Black") then takeDamage() end end)
 end
 
-local function updatePlayer()
+local function updatePlayer(colorPlayer)
     Global.call("setUI", colorPlayer) Global.call("updateSave")
 end
 local function reCalculatePlayer()
-    Global.call("calculateInfo", colorPlayer) updatePlayer()
+    Global.call("calculateInfo", selectedColorPlayer) updatePlayer(selectedColorPlayer)
 end
 
 function takeDamage()
-    local player = Global.getVar("saveInfoPlayer")[colorPlayer]
+    local player = Global.getVar("saveInfoPlayer")[selectedColorPlayer]
     player.Health.current = player.Health.current - 1
-    updatePlayer()
+    updatePlayer(selectedColorPlayer)
 end
 
 function changeRace(player, race)
-    local player = Global.getVar("saveInfoPlayer")[colorPlayer]
+    local player = Global.getVar("saveInfoPlayer")[selectedColorPlayer]
     player.Race = race
-    Global.call("changeRaceBonus", colorPlayer)
+    Global.call("changeRaceBonus", selectedColorPlayer)
     Wait.time(|| reCalculatePlayer(), 0.2)
 end
 
 function changeClass(player, class)
-    local player = Global.getVar("saveInfoPlayer")[colorPlayer]
+    local player = Global.getVar("saveInfoPlayer")[selectedColorPlayer]
     player.Class = class
-    Global.call("changeClassBonus", colorPlayer)
-    Wait.time(|| Global.call("sortSkillsByImportance", colorPlayer), 0.2)
+    Global.call("changeClassBonus", selectedColorPlayer)
+    Wait.time(|| Global.call("sortSkillsByImportance", selectedColorPlayer), 0.2)
     Wait.time(|| reCalculatePlayer(), 0.3)
 end
 
 function changeColor(player, color)
-    colorPlayer = color
+    selectedColorPlayer = color
 end
 
 function fieldEdit(player, value, id)
     self.UI.setAttribute(id, "text", value)
 end
 
+-- Check for exceeding limits
+local function checkValue(current, max)
+    return current > max and max or current < 1 and 0 or current
+end
+-- Restore all statistical parameters to maximum
+local function restoreAll(player)
+    player.Health.current = player.Health.max
+    player.Mana.current = player.Mana.max
+    player.Stamina.current = player.Stamina.max
+end
+-- Stamina regeneration function after each move
+local function restoreStaminaPerTurn()
+    local indexSleep = 0.1
+    for colorPlayer, player in pairs(Global.getVar("saveInfoPlayer")) do
+        player.Stamina.current = player.Stamina.current + math.floor(2.5 + (0.02 * player.Characteristics.Endurance))
+        checkValue(player.Stamina.current, player.Stamina.max)
+        Wait.time(|| updatePlayer(colorPlayer), indexSleep)
+        indexSleep = indexSleep + 0.1
+    end
+end
 function changePlayerState(player, alt, id)
-    local player = Global.getVar("saveInfoPlayer")[colorPlayer]
+    local player = Global.getVar("saveInfoPlayer")[selectedColorPlayer]
     if(id == "restoreAll") then
-        player["Health"].current = player["Health"].max
-        player["Mana"].current = player["Mana"].max
-        player["Stamina"].current = player["Stamina"].max
+        restoreAll(player)
+    elseif(id == "restoreStaminaPerTurn") then
+        restoreStaminaPerTurn()
     else
         local state = self.UI.getAttribute(id, "text")
         local value = tonumber(self.UI.getAttribute(state, "text")) * (alt == "-1" and 1 or -1)
         player[state].current = player[state].current + value
-        player[state].current = player[state].current > player[state].max and player[state].max or player[state].current < 1 and 0 or player[state].current
+        checkValue(player[state].current, player[state].max)
     end
-    updatePlayer()
+    updatePlayer(selectedColorPlayer)
+end
+
+function whatPartBodyHit()
+    local roll = math.random(1, 100)
+    local cumulativeProbability = 0
+    for bodyPart, probability in pairs(bodyHitTable) do
+        cumulativeProbability = cumulativeProbability + probability
+        if roll <= cumulativeProbability then
+            if(bodyPart == "chest" or bodyPart == "head") then
+                print("The impact came on: " .. bodyPart)
+            else
+                local whatBP = string.format("%s %s", math.random(1, 2) == 1 and "left" or "right", bodyPart)
+                print("The impact came on: " .. whatBP)
+            end
+            return
+        end
+    end
 end
